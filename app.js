29bfb51df2a80118cfa015b10a631c721f992a74
Luna-rmsoft <88071251+arty0928@users.noreply.github.com>
2023-01-19 17:49:26 +0900

    #10

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
+"use strict";
+
+//모듈
 const express = require('express');
 const app = express();
 
-app.listen(3000, () =>{
-    console.log('서버 가동');
-});
+//라우팅
+const home = require("./routes/home");
+
+//app setting
+app.set("views","./views");
+app.set("view engine", "ejs");
+
+app.use("/", home); //use -> 미들웨어 등록 메소드
+
+module.exports = app;
+
