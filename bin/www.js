3229167b4008cb2a77df5a4dd411fe01083ad776
Luna-rmsoft <88071251+arty0928@users.noreply.github.com>
2023-01-30 10:54:29 +0900

    git

--- bin/www.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const app = require("../app");
-const PORT = 3000;
-
-//listen 이 함수가 서버에서 실행해서 화면에 보여줌
-app.listen(PORT, () =>{
-    console.log('서버 가동');
-});
---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://arty0928:park3877!!@simpleboardcluster.pag7qlh.mongodb.net/?retryWrites=true&w=majority')
+.then(()=>console.log('MongoDB Connected'))
+.catch(err => console.log(err));
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
