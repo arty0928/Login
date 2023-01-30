4412f7ed8e5b5187424cab3b75c8c16987895aa0
Luna-rmsoft <88071251+arty0928@users.noreply.github.com>
2023-01-30 22:36:21 +0900

    "token"

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -27,6 +30,47 @@ const userSchema = mongoose.Schema({
     }
 
 })
+
+userSchema.pre('save', function(next){
+
+    var user = this;
+
+    if(user.isModified('password')){
+        //비번 암호화
+        bcrypt.genSalt(saltRounds, function(err, salt){
+            if(err) return next(err)
+
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if(err) return next(err);
+                user.password = hash;
+                next();
+            })
+        })
+    }else{
+        next();
+    }
+})
+
+userSchema.methods.comparePassword = function(plainPassword, cb){
+    //plainpassword 1234567 === 암호화된 비번 "$2b$10$1bWGrIolGafj2Tbsv2VUbOn2tQmi8sVGGTHeap2ImLfG48Bxd7HtG"
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+        if(err) return cb(err);
+            cb(null, isMatch);
+    });
+};
+
+userSchema.methods.generateToken = function(cb){
+    var user = this;
+
+    //jsonwebtoken을 이용해서 token을 생성하기
+    var token = jwt.sign(user._id.toHexString(),'secretToken')
+    
+    user.token = token;
+    user.save(function(err, user){
+        if(err) return cb(err);
+        cb(null, user)
+    })
+}
 
 const User = mongoose.model('User',userSchema);
 module.exports = {User};
