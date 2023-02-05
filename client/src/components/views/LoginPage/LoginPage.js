9b9786536a6423328976cfe31cbf2dbc6fa26426
Luna-rmsoft <88071251+arty0928@users.noreply.github.com>
2023-02-05 22:00:17 +0900

    login

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,10 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function LoginPage() {
+
+  const [Email,setEmail] =  useState("");
+  const [Password,setPassword] =  useState("");
+  
+  const onEmailHandler = (event) =>{
+    setEmail(event.currentTarget.value);
+  }
+
+  const onPasswordHandler = (event) => {
+    setPassword(event.currentTarget.value);
+  }
+
+  const onSubmitHandler = (event) => {
+    
+  }
+
+
   return (
-    <div>
-      LoginPage
-    </div>
+    <div style ={{
+      display: 'flex', justifyContent: 'center', alignItems: 'center',
+                  width: '100%', height: '100vh'}}>
+    
+
+    <form style={{display: 'flex', flexDirection: 'column'}}
+      onSubmit={onSubmitHandler}
+    >
+        <label>Email</label>
+        <input type="email" value={Email} onChange ={onEmailHandler}/>
+        <label>Password</label>
+        <input type="password" value ={Password} onChange={onPasswordHandler}/>
+
+        <button type="submit">
+          Login
+        </button>
+    </form>
+
+  </div>
   )
 }
 
