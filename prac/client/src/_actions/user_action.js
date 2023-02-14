774c759ba1373b453a73ee85446c46cda72630f7
Luna-rmsoft <88071251+arty0928@users.noreply.github.com>
2023-02-14 18:03:38 +0900

    "check"

--- prac/client/src/_actions/user_action.js
+++ prac/client/src/_actions/user_action.js
@@ -10,8 +10,12 @@ export function loginUser(dataToSubmit){
 
     const request = axios.post('/api/user/login',dataToSubmit)
     //서버에서 받은 데이터를 request에 저장
-        .then(response =>response.data)
+    //서버에서 로그인 정보(loginSuccess: true, userId: ~~)를 쿠키에 저장해서 response로 보냄 -> response.data가 request로
+    //response.data : loginSuccess: true, userId: ~~
+        .then(response => response.data);
     
+    // console.log(request); //promise (axios사용해서)
+
     //return해서 reducer에 넘겨줘야
     //reducer: pre와 next action을 조합해서 다음 state을 만들어야 
     return {
@@ -24,6 +28,7 @@ export function registerUser(dataToSubmit){
 
     const request = axios.post('/api/user/register',dataToSubmit)
     //서버에서 받은 데이터를 request에 저장
+    //console.log(response.data) : {success: true} : server에서 json 타입으로 보낸 response
         .then(response =>response.data)
     
     console.log(`registerUser: request: ${request}`);
